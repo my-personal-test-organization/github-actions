f1ed304b114d439fcd55c049995eed8ebb705bf3
vitaly <vitalikstain@inbox.ru>
2024-10-28 16:42:38 +0300

    [upd] test files

--- test_file_1.py
+++ test_file_1.py
@@ -1,1 +1,1 @@
-#test file 1 from test branch try to test 21
+#test file 1 from test branch try to test 22
--- test_file_2.py
+++ test_file_2.py
@@ -1,1 +1,1 @@
-#test file 2 from test branch try to test 21
+#test file 2 from test branch try to test 22
